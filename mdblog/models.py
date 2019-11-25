70348fae4434628422d7f725acceca9a63eb916f
Richardmisko <richardmisko@yahoo.com>
2019-11-25 11:21:26 +0000

    using sqlalchemy as ORM

---
+++ mdblog/models.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy   # 301 , 3:20
+
+db = SQLAlchemy()
+
+class Article(db.Model): 
+	id = db.Column(db.Integer, primary_key = True)
+	title = db.Column(db.String)
+	content = db.Column(db.String)
+
+# 301, 3:20
+
